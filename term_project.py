52e56435ccc71053a4687ae102a8686a811718ae
Austin Bursey <burs5200@mylaurier.ca>
2019-11-29 16:38:47 -0500

    Updated Project

--- term_project.py
+++ term_project.py
@@ -6,13 +6,10 @@ Email:  burs5200@mylaurier.ca
 __updated__ = "2019-09-23"
 ------------------------------------------------------------------------
 """
-# i need these libraries
+
 import pandas 
 import matplotlib.pyplot as plt
 from sklearn import model_selection
-
-
-#i think i need these libraries 
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -20,90 +17,12 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 from imblearn.over_sampling import ADASYN, SMOTE
-#why are they here 
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.svm import SVC
-import seaborn as sns
+from sklearn.model_selection import GridSearchCV
+import numpy as np
 from math import sqrt
 
-def findOptimalK(desc_train,targ_train):
-    scoring = 'accuracy'
-    i_array = list()
-    euclid = list()
-    euclid_W = list()
-    manhattan = list()
-    manhattan_W = list()
-    Gaussian = list()
-    for i in range(1,10):
-        models =[]
-        models.append(('KNN-Euclid', KNeighborsClassifier(n_neighbors=i, p=2)))
-        models.append(("KNN-Euclid-Weighted", KNeighborsClassifier(n_neighbors=i,weights='distance',p=2 )))
-        models.append(('KNN-Manhattan', KNeighborsClassifier(n_neighbors=i, p=1)))
-        models.append(("KNN-Manhattan-Weighted", KNeighborsClassifier(n_neighbors=i,weights='distance',p=1 )))    
-        #models.append(("Gaussian Bayes", GaussianNB()))
 
-        # evaluate each model in turn
-        results = []
-        names = []
-        for name, model in models:
-            models
-            kfold = model_selection.StratifiedKFold(n_splits=splits, shuffle=True, random_state=seed)
-            cv_results = model_selection.cross_val_score(model, desc_train, targ_train, cv=kfold, scoring=scoring)
-            results.append(cv_results)
-            names.append(name)
-            msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
-            print(msg)
-            print("-")
-            if name == 'KNN-Euclid': 
-                euclid.append( cv_results.mean())
-            elif name == "KNN-Euclid-Weighted": 
-                euclid_W.append( cv_results.mean())
-            elif name == 'KNN-Manhattan': 
-                manhattan.append( cv_results.mean())
-            elif name == 'KNN-Manhattan-Weighted': 
-                manhattan_W.append( cv_results.mean())
-            # elif name == 'Gaussian Bayes': 
-            #     Gaussian.append( cv_results.mean())
-                        
-
-    plt.plot(i,euclid)
-    plt.plot(i,euclid_W)
-    plt.plot(i,manhattan)
-    plt.plot(i,manhattan_W)
-    plt.plot(i,Gaussian)
-
-    plt.legend('Euclidian','Weighted Euclidian','Manhattan', 'Manhattan Weighted', 'Guassian',loc='upper left')
-
-    plt.show()
-
-if __name__ == "__main__": 
-    '''
-    Variables 
-    To 
-    Change 
-    Between
-    Runs
-    '''
-    validation_size = 0.1
-    seed = 58
-    neighbhors = 10
-    splits =10
-
-
-    # Load dataset
-    url = "./krkopt.csv"
-    names = ["White-King-Col", "White-King-Row", "White-Rook-Col", "White-Rook-Row","Black-King-Col", "Black-King-Row","Moves till Checkmate" ]
-    dataset = pandas.read_csv(url, names=names)
-    array = dataset.values
-    
-    descriptiveFeats = array[:,0:6]
-    targetFeats = array[:,6]
-
-    sm = SMOTE(random_state=seed)
-    descriptiveFeats, targetFeats =sm.fit_resample(descriptiveFeats,targetFeats)
-
+def VisualizeDataset(dataset): 
     '''
     Visualize
     And
@@ -128,10 +47,107 @@ if __name__ == "__main__":
     dataset.hist()
     plt.show()
 
+    #Correlation Matrix
+    pandas.plotting.scatter_matrix(dataset,  figsize=(6, 6))
+    plt.show()
+
+def plotOptimalK(desc_train,targ_train,splits):
+    scoring = 'accuracy'
+    i_array = list()
+    euclid = list()
+    euclid_W = list()
+    manhattan = list()
+    manhattan_W = list()
+    Gaussian = list()
+    for i in range(1,50):
+        models =[]
+        models.append(('KNN-Euclid', KNeighborsClassifier(n_neighbors=i, p=2)))
+        models.append(("KNN-Euclid-Weighted", KNeighborsClassifier(n_neighbors=i,weights='distance',p=2 )))
+        models.append(('KNN-Manhattan', KNeighborsClassifier(n_neighbors=i, p=1)))
+        models.append(("KNN-Manhattan-Weighted", KNeighborsClassifier(n_neighbors=i,weights='distance',p=1 )))    
+        #models.append(("Gaussian Bayes", GaussianNB()))
+
+        # evaluate each model in turn
+        results = []
+        names = []
+        for name, model in models:
+            models
+            kfold = model_selection.StratifiedKFold(n_splits=splits,  random_state=seed)
+            cv_results = model_selection.cross_val_score(model, desc_train, targ_train, cv=kfold, scoring=scoring)
+            results.append(cv_results)
+            names.append(name)
+            # msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+            # print(msg)
+            # print("-")
+            if name == 'KNN-Euclid': 
+                euclid.append( cv_results.mean())
+            elif name == "KNN-Euclid-Weighted": 
+                euclid_W.append( cv_results.mean())
+            elif name == 'KNN-Manhattan': 
+                manhattan.append( cv_results.mean())
+            elif name == 'KNN-Manhattan-Weighted': 
+                manhattan_W.append( cv_results.mean())
+            # elif name == 'Gaussian Bayes': 
+            #     Gaussian.append( cv_results.mean())
+        
+        i_array.append(i)
+
+    plt.plot(i_array,euclid, label= 'Euclidian')
+    plt.plot(i_array,euclid_W, label= 'Euclidian Weighted')
+    plt.plot(i_array,manhattan, label= 'Manhattan ')
+    plt.plot(i_array,manhattan_W, label= 'Manhattan Weighted')
+
+    plt.legend( loc='upper left')
+    plt.title("Accuracy per (K)")
+    plt.xlabel("K values")
+    plt.ylabel("Accuracy")
+    plt.show()
+
+if __name__ == "__main__": 
+    '''
+    Variables 
+    To 
+    Change 
+    Between
+    Runs
+    '''
+    validation_size = 0.1
+    seed = 58
+    neighbhors = 8
+    splits =100
 
 
-    desc_train, desc_validation, targ_train, targ_validation = model_selection.train_test_split(descriptiveFeats, targetFeats, test_size=validation_size, random_state=seed,stratify=targetFeats)
+    # Load dataset
+    url = "./krkopt.csv"
+    names = ["White-King-Col", "White-King-Row", "White-Rook-Col", "White-Rook-Row","Black-King-Col", "Black-King-Row","Moves till Checkmate" ]
+    dataset = pandas.read_csv(url, names=names)
+    array = dataset.values
+    
+    descriptiveFeats = array[:,0:6]
+    targetFeats = array[:,6]
 
+    sm = SMOTE(random_state=seed)
+    descriptiveFeats, targetFeats =sm.fit_resample(descriptiveFeats,targetFeats)
+    '''
+    Visualize
+    And
+    Summarize
+    The Dataset
+    '''
+    VisualizeDataset(dataset)
+
+    scoring = 'accuracy'
+    #desc_train, desc_validation, targ_train, targ_validation = model_selection.train_test_split(descriptiveFeats, targetFeats, test_size=validation_size, random_state=seed,stratify=targetFeats)
+    
+    '''
+    Find
+    Optimal 
+    K
+    '''
+    #findOptimalK(desc_train,targ_train)
+    
+    
+    
     models =[]
     models.append(('KNN-Euclid', KNeighborsClassifier(n_neighbors=neighbhors, p=2)))
     models.append(("KNN-Euclid-Weighted", KNeighborsClassifier(n_neighbors=neighbhors,weights='distance',p=2 )))
@@ -139,25 +155,23 @@ if __name__ == "__main__":
     models.append(("KNN-Manhattan-Weighted", KNeighborsClassifier(n_neighbors=neighbhors,weights='distance',p=1 )))    
     models.append(("Gaussian Bayes", GaussianNB()))
 
-    # evaluate each model in turn
+    #evaluate each model in turn
     results = []
     names = []
     for name, model in models:
-        models
         kfold = model_selection.StratifiedKFold(n_splits=splits, shuffle=True, random_state=seed)
-        cv_results = model_selection.cross_val_score(model, desc_train, targ_train, cv=kfold, scoring=scoring)
+        cv_results = model_selection.cross_val_score(model, descriptiveFeats, targetFeats, cv=kfold, scoring=scoring)
         results.append(cv_results)
         names.append(name)
         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
         print(msg)
 
 
-
-    for name, model in models: 
-        print("-----{}------".format(name))
-        model.fit(desc_train,targ_train)
-        predictions = model.predict(desc_validation)
-        print(accuracy_score(targ_validation, predictions))
-        print(confusion_matrix(targ_validation, predictions))
-        print(classification_report(targ_validation, predictions))
-        print()
+    # for name, model in models: 
+    #     print("-----{}------".format(name))
+    #     model.fit(desc_train,targ_train)
+    #     predictions = model.predict(desc_validation)
+    #     print(accuracy_score(targ_validation, predictions))
+    #     print(confusion_matrix(targ_validation, predictions))
+    #     print(classification_report(targ_validation, predictions))
+    #     print()
